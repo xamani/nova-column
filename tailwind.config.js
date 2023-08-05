871c569b64f8b5bbdc372e4590e2488b8e044fc2
Hamed Xamani <xamanihamed@gmail.com>
2023-08-05 17:40:32 +0330

    Revert "Fixes for 4.26.5"
    This reverts commit 42593115c699b8da2cd403fbc44528b9196c1ffd.

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,6 +30,5 @@ module.exports = {
     'flex-wrap',
     'px-8',
     'mt-2',
-    'py-4',
   ],
 };
--- webpack.mix.js
+++ webpack.mix.js
@@ -19,5 +19,4 @@ mix
   .alias({
     'laravel-nova': path.join(__dirname, 'vendor/laravel/nova/resources/js/mixins/packages.js'),
     nova: path.join(__dirname, 'vendor/laravel/nova/resources/js'),
-    '@': path.join(__dirname, 'vendor/laravel/nova/resources/js'),
   });
